4f6f5254b24d268449db8574a3c569361d5ca7bf
John Trujillo <amriikk@gmail.com>
2021-03-01 22:33:31 -0800

    update roulete input

--- HW/Ch3HW.py
+++ HW/Ch3HW.py
@@ -20,14 +20,14 @@ if BAD: # Boolean Variable
     print("You must enter PRIMARY colors. Please try again!")
 elif color2 == color1:
     print("You must enter two DISTINCT primary colors. ")
-elif color1 == "red" and color2 == "yellow":
-    print("Red + Yellow => Orange.")
-elif color1 == "yellow" and color2 == "red":
-    print("Yellow + Red => Orange.")
 elif color1 == "red" and color2 == "blue":
     print("Red + Blue => Purple.")
 elif color1 == "blue" and color2 == "red":
     print("Blue + Red => Purple.")
+elif color1 == "red" and color2 == "yellow":
+    print("Red + Yellow => Orange.")
+elif color1 == "yellow" and color2 == "red":
+    print("Yellow + Red => Orange.")
 elif color1 == "blue" and color2 == "yellow":
     print("Blue + Yellow => Green.")
 else:
@@ -37,6 +37,7 @@ print('\n')
 
 # 9 -- Roulette Wheel Colors
 
+num = int(input('Enter a pocket number from 0 - 36: '))
 
 
 print('\n')
