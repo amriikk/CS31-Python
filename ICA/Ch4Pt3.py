e3737b8b987a5e5d18a1098656203a7193636ed1
John Trujillo <amriikk@gmail.com>
2021-03-03 19:41:37 -0800

    update drawings

--- ICA/Ch4Pt3.py
+++ ICA/Ch4Pt3.py
@@ -14,7 +14,6 @@
 # ICA 3, Part 2
 
 BASE_SIZE = 8
-
 for row in range(BASE_SIZE, 0, -1):
     for col in range(row+1):
         print('*', end='')
@@ -22,9 +21,17 @@ for row in range(BASE_SIZE, 0, -1):
 print()
 
 NUM_STEPS = 6
-
 for row in range(NUM_STEPS, 0, -1):
     for col in range(row-1):
         print(' ', end='')
     print('#')
 print()
+
+# ICA 3, Part 3
+
+for x in range(7):
+    for y in range(x):
+        print('$', end='')
+    for z in range(6-x):
+        print('#', end='')
+    print()
