5f33f4d401f04a5824b69e4d4313b3e4b0569ee6
John Trujillo <amriikk@gmail.com>
2021-03-03 19:32:55 -0800

    update part II

--- ICA/Ch4Pt3.py
+++ ICA/Ch4Pt3.py
@@ -1,14 +1,24 @@
 # ICA 3, Part 1
 
-players = int(input('Enter number of players: '))
-seasons = int(input('Enter number of seasons: '))
+# players = int(input('Enter number of players: '))
+# seasons = int(input('Enter number of seasons: '))
 
-for x in range(players):
-    total = 0
-    name = input('Enter the name of a hockey player ')
-    for y in range(1, seasons+1):
-        goals = int(input(f'Enter goals scored in season #{y}: '))
-        total += goals 
-    print(name, 'has scored', total, "goals in the last", seasons, "seasons.")
+# for x in range(players):
+#     total = 0
+#     name = input('Enter the name of a hockey player ')
+#     for y in range(1, seasons+1):
+#         goals = int(input(f'Enter goals scored in season #{y}: '))
+#         total += goals 
+#     print(name, 'has scored', total, "goals in the last", seasons, "seasons.")
 
-#
+# ICA 3, Part 2
+
+BASE_SIZE = 8
+
+for row in range(BASE_SIZE, 0, -1):
+    for col in range(row+1):
+        print('*', end='')
+    print()
+print()
+
+NUM_STEPS = 6
