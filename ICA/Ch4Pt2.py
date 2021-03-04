4efee7dad658de14ac7aaf7903c92372ac4c04a1
John Trujillo <amriikk@gmail.com>
2021-03-03 18:51:03 -0800

    finalize ICA 2

--- ICA/Ch4Pt2.py
+++ ICA/Ch4Pt2.py
@@ -1,13 +1,18 @@
-# ICA 2, Part 1
+# ICA 2, Part 1 + 3
 
-# total = 0
-# howMany = int(input('How many numbers do you want? '))
-# for x in range(1, howMany+1):
-#     num = int(input(f'Enter number #{x}: '))
-#     total += num
-# print(f'The average is {total/howMany:.3f}')
+total = 0
+howMany = int(input('How many numbers do you want? '))
 
-# print()
+while howMany <= 0:
+    print('Must be positive. Try again! ')
+    howMany = int(input('How many numbers do you want? '))
+for x in range(1, howMany+1):
+    num = int(input(f'Enter number #{x}: '))
+    total += num
+
+print(f'The average is {total/howMany:.3f}')
+
+print()
 
 
 # ICA 2, Part 2
@@ -15,8 +20,11 @@
 total = 0
 how_Many = 0
 num = int(input('Enter a number: '))
+
 while num != -1:
     total += num
     how_Many += 1
     num = int(input('Enter a number or -1 to quit: '))
+
 print(f'The average is {total/how_Many:.3f}')
+print()
