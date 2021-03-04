00e10df13c6bfb87416ea08642a24b723dbd5569
John Trujillo <amriikk@gmail.com>
2021-03-03 19:10:33 -0800

    udpate ICA 3, part 1

--- ICA/Ch4Pt3.py
+++ ICA/Ch4Pt3.py
@@ -1,1 +1,12 @@
 # ICA 3, Part 1
+
+players = int(input('Enter number of players: '))
+seasons = int(input('Enter number of seasons: '))
+
+for x in range(players):
+    total = 0
+    name = input('Enter the name of a hockey player ')
+    for y in range(1, seasons+1):
+        goals = int(input(f'Enter goals scored in season #{y}: '))
+        total += goals 
+    print(name, 'has scored', total, "goals in the last", seasons, "seasons.")
