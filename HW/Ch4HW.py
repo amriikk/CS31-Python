d3825403aecb0a535a6ce89a96d7e9d92ea4d8ad
John Trujillo <amriikk@gmail.com>
2021-03-07 19:26:42 -0800

    update for loop !

--- HW/Ch4HW.py
+++ HW/Ch4HW.py
@@ -6,19 +6,20 @@
 # ####################################################
 
 ## 12 -- Calculating the Factorial of a Number
-
+print()
 num = int(input("Enter a Positive Integer: "))
 print()
-factorial = 1
+
 
 if num < 0:
     print('Factorial not defined for negative numbers. Try again!')
 elif num == 0:
     print(f'The Factorial of {num} is 1')
 else:
+    factorial = 1
     for x in range(1, num+1):
-        factorial *= num
-        print(f'The factorial of {num:.3f} is {factorial}')
+        factorial = factorial * x
+    print(f'{num}! is equal to {factorial}')
 print()
 
 
