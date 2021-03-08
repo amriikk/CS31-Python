e7ef03a10e206fe2aa08d7508f2f74aff2cb2391
John Trujillo <amriikk@gmail.com>
2021-03-07 21:14:30 -0800

    uncomment code

--- HW/Ch4HW.py
+++ HW/Ch4HW.py
@@ -7,21 +7,21 @@
 
 ## 12 -- Calculating the Factorial of a Number
 
-# print()
-# num = int(input("Enter a Positive Integer: "))
-# print()
+print()
+num = int(input("Enter a Positive Integer: "))
+print()
 
 
-# if num < 0:
-#     print('Factorial not defined for negative numbers. Try again!')
-# elif num == 0:
-#     print(f'The Factorial of {num} is 1')
-# else:
-#     factorial = 1
-#     for x in range(1, num+1):
-#         factorial = factorial * x
-#     print(f'{num}! is equal to {factorial}')
-# print()
+if num < 0:
+    print('Factorial not defined for negative numbers. Try again!')
+elif num == 0:
+    print(f'The Factorial of {num} is 1')
+else:
+    factorial = 1
+    for x in range(1, num+1):
+        factorial = factorial * x
+    print(f'{num}! is equal to {factorial}')
+print()
 
 
 ## 13 -- Population
@@ -44,3 +44,5 @@ for x in range(n_organisms, n_days+1):
 print()
 
 ## 15 -- Loop Pattern
+
+print()
