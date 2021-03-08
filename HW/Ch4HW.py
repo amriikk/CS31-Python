eca18b6c42a26b1cf3dbaaeebacb09b06d0d5741
John Trujillo <amriikk@gmail.com>
2021-03-07 19:15:49 -0800

    update #12 factorial

---
+++ HW/Ch4HW.py
@@ -0,0 +1,28 @@
+##################################################### 
+# CS 31, Prof. Muldrow
+# Name: Jhon Trujillo
+# Assignment: Ch4_HW
+# Due Date: 03.07.21
+# ####################################################
+
+## 12 -- Calculating the Factorial of a Number
+
+num = int(input("Enter a Positive Integer: "))
+print()
+factorial = 1
+
+if num < 0:
+    print('Factorial does not exist for negative numbers. Try again!')
+elif num == 0:
+    print(f'The Factorial of {num} is 1')
+else:
+    for x in range(1, num+1):
+        factorial *= num
+        print(f'The factorial of {num:.3f} is {factorial}')
+print()
+
+
+## 13 -- Population
+
+
+## 15 -- Loop Pattern
