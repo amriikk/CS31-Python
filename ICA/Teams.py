4b51916de24c7d1e65a5a19ac1f745f877b78239
John Trujillo <amriikk@gmail.com>
2021-03-08 18:57:53 -0800

    update Teams functions

---
+++ ICA/Teams.py
@@ -0,0 +1,31 @@
+# ICA 1 -- Teams
+
+def main():
+    kings()
+    ducks()
+    knights()
+    yotes()
+
+def kings():
+    print('Los Angeles Kings - began NHL play in 1967')
+    print('Home arena is Staple Center')
+    print()
+
+def ducks():
+    print('Anaheim Ducks - began NHL play in 1993')
+    print('Home arena is Honda Center')
+    print()
+
+def knights():
+    print('Vegas Golden Knights - began NHL play in 2017')
+    print('Home arena is T-Mobile Center')
+    print()
+
+def yotes():
+    print('Arizona Coyotes - began NHL play in 1979 as Winnipeg Jets')
+    print('Moved to Arizona in 1996')
+    print('Home arena is Gila River Center')
+    print()
+
+# Call main()
+main()
