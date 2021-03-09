d7b46bd7b6cb9cbec36dd46d9c21a6773f17ebd9
John Trujillo <amriikk@gmail.com>
2021-03-08 19:32:51 -0800

    finalize ICA

--- ICA/Teams.py
+++ ICA/Teams.py
@@ -1,10 +1,18 @@
 # ICA 1 -- Teams
-
 def main():
-    kings()
-    ducks()
-    knights()
-    yotes()
+    team = int(input('Type 1 for Kings, 2 for Ducks, 3 for Knights, 4 for Coyotes or anything else to quit: '))
+    while team >= 1 and team <= 4:
+        if team == 1:
+            kings()
+        elif team == 2:
+            ducks()
+        elif team == 3:
+            knights()
+        else:
+            yotes()
+        input('Press Enter to continue..')
+        team = int(input('Type 1 for Kings, 2 for Ducks, 3 for Knights, 4 for Coyotes or anything else to quit: '))
+    print('That\'s all!')
 
 def kings():
     print('Los Angeles Kings - began NHL play in 1967')
@@ -28,4 +36,5 @@ def yotes():
     print()
 
 # Call main()
+print()
 main()
