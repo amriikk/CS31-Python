db46fdceb3ec95b60ffafa012b9bbe7a73c7beda
John Trujillo <amriikk@gmail.com>
2021-03-10 19:03:56 -0800

    update parameter

--- ICA/Ch5Part2.py
+++ ICA/Ch5Part2.py
@@ -2,10 +2,23 @@
 
 def main():
     kms = float(input('Enter kilometers: '))
-    getMiles(kms)
+    getMiles(kms, convert=0.6214)
+    print()
+    fatGrams = int(input('Enter fat grams consumed today: '))
+    carbGrams = int(input('Enter carbohydrate grams consumed today: '))
+    getCalories(fatGrams, carbGrams)
 
-def getMiles(k):
-    miles = k * 0.6214
+def getMiles(k, convert):
+    miles = k * convert
     print(f'{k:.1f} kilometers = {miles:.1f} miles.')
+
+def getCalories(fat, carb):
+    fat_calories = fat * 9
+    carb_calories = carb * 4
+    total_cals = fat_calories + carb_calories
+    print('\nConsuming', fat, 'fat grams gives you', fat_calories, 'calories from fat.')
+    print('Consuming', carb, 'carb grams gives you', carb_calories, 'calories from carbs.')
+    print('Total calories from fat and carbs today is', total_cals)
+
 
 main()
