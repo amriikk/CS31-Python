d35b23310afc5b0b07dc46e795b5b933f7539583
John Trujillo <amriikk@gmail.com>
2021-03-15 19:21:31 -0700

    update toss function

--- ICA/Ch5Part3.py
+++ ICA/Ch5Part3.py
@@ -6,11 +6,23 @@ total = 0
     for x in range(1,6):
         #random.seed(random.uniform(2.5,125.3))
         slot = random.randrange(1,10)
-        amt
-        print(f'Disk #{x}: You won ${money}')
+        amt = plinkToss(slot)
+        print(f'Disk #{x}: You won ${amt} (slot = {slot})')
+        total = total + amt
         
         
-        if slot == 1 or slot == 9:
-            money = 1
+        
         
 
+def plinkToss(slot):
+    if slot == 1 or slot == 9:
+        money = 100
+    elif slot == 2 or slot == 8:
+        money = 500
+    elif slot == 3 or slot == 7:
+        money == 1000
+    elif slot == 5:
+        money == 10000
+    else: 
+        moneyn = 0
+    return money
