d82075f5c5f02728d92d4fef3fdba2c4be2e08c2
John Trujillo <amriikk@gmail.com>
2021-03-15 19:14:48 -0700

    update ICA 1 for week

---
+++ ICA/Ch5Part3.py
@@ -0,0 +1,16 @@
+# Week 6, ICA 1
+import random
+# Let's play Plinko!!
+def main():
+total = 0
+    for x in range(1,6):
+        #random.seed(random.uniform(2.5,125.3))
+        slot = random.randrange(1,10)
+        amt
+        print(f'Disk #{x}: You won ${money}')
+        
+        
+        if slot == 1 or slot == 9:
+            money = 1
+        
+
