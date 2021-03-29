8b0d106a923c9882093745ade54d8bf31c4a4914
John Trujillo <amriikk@gmail.com>
2021-03-29 00:08:55 -0700

    refactor game

--- HW/scratchPaper.py
+++ HW/scratchPaper.py
@@ -11,28 +11,36 @@ def gamePlay():
     tie = 0
 
     # Loop GamePlays.
-    while True:
-        print(f'Win: {win} | Losses: {loss} | Ties: {tie}')
-        print("""\nEnter Your Move:\n
-            [1] - Rock 
-            [2] - Paper 
-            [3] - Scissors 
-            [4] - Quit Game\n""")
-        uSelection = int(input('\nType your Selection: '))
+    # while True:
+    print(f'Win: {win} | Losses: {loss} | Ties: {tie}')
+    print("""\nEnter Your Move:\n
+        [1] - Rock 
+        [2] - Paper 
+        [3] - Scissors 
+        [4] - Quit Game\n""")
+    uSelection = int(input('\nType your Selection: '))
 
-        userMove = userSelection()
-        cpuMove = randomCPU()
-        checkWins()
+    userMove = userSelection(uSelection)
+    cpuMove = randomCPU()
+    tallyCount = checkWins(userMove, cpuMove)
+
+    if tally == 1:
+        tie += 1
+    elif tally == 2:
+        win += 1
+    elif tally == 3:
+        loss += 1
     
-    replay = input('\nPlay Again (y/n)?: ').lower
-    if replay == 'y':
-        print('Replay!\n')
-        gamePlay()
-    else:
-        print('Thanks for playing!\n')
+    
+    # replay = input('\nPlay Again (y/n)?: ').lower
+    # if replay == 'y':
+    #     print('Replay!\n')
+    #     gamePlay()
+    # else:
+    #     print('Thanks for playing!\n')
 
 # User Selection method
-def userSelection():    
+def userSelection(uSelection):    
     if uSelection == 1:
         userMove = 'r'
         print("Your Selection:\tRock")
@@ -73,28 +81,35 @@ def randomCPU():
         return cpuMove
 
 # Check Win + Game Counters
-def checkWins():
+def checkWins(userMove, cpuMove):
     if userMove == cpuMove:
         print("It is tie:!")
-        tie += 1
+        tie = 1
+        return tie
     elif userMove == 'r' and cpuMove == 's':
         print("You Win!")
-        win += 1
+        win = 2
+        return win
     elif userMove == "p" and cpuMove == 'r':
-        win += 1
+        win = 2
         print("You Win!")
+        return win
     elif userMove == "s" and cpuMove == 'p':
-        win += 1
+        win = 2
         print("You Win!")
+        return win
     elif userMove == "r" and cpuMove == 'p':
-        loss += 1
+        loss = 3
         print("You Lose!")
+        return loss
     elif userMove == "p" and cpuMove == 's':
-        loss += 1
+        loss = 3
         print("You Lose!")
+        return loss
     elif userMove == "s" and cpuMove == 'r':
-        loss += 1
+        loss = 3
         print("You Lose!")
+        return loss
 
 ## Solution 21
 gamePlay()
