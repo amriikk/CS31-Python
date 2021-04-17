7787d5559a1af17a072607e640fe4feadcfd0ff2
John Trujillo <amriikk@gmail.com>
2021-04-17 01:26:46 -0700

    Ch6HW due 04/18

---
+++ HW/Ch6HW.py
@@ -0,0 +1,12 @@
+##################################################### 
+# CS 31, Prof. Muldrow
+# Name: Jhon Trujillo
+# Assignment: Ch6_HW
+# Due Date: 04.18.21
+# ####################################################
+
+## Problems 6 + 9 - Average of Numbers | Exception Handling
+
+## Problems 7 + 8 - Random Number File Writer | Reader
+
+## Problem 10 - Golf Scores
