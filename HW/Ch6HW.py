d275358c7dc0f375ec84399de3238fb3e5af04dd
John Trujillo <amriikk@gmail.com>
2021-04-18 23:44:04 -0700

    update Ch6HW

--- HW/Ch6HW.py
+++ HW/Ch6HW.py
@@ -6,7 +6,21 @@
 # ####################################################
 
 ## Problems 6 + 9 - Average of Numbers | Exception Handling
+def displayData():
+    infile = open('nba.txt','r')
+    for name in infile:
+        name = name.rstrip('\n')
+        num = infile.readline().rstrip('\n')
+        team = infile.readline().rstrip('\n')
+        print(name,'wears number',num,'and plays for the',team)
+    infile.close()
 
 ## Problems 7 + 8 - Random Number File Writer | Reader
+outfile = open("random.txt", 'w')
+rep = int(input("Enter amount of random numbers to generate: "))
+for x in range(1,rep+1):
+    number = random.randrange(501)
+    outfile.write(number+"\n")
+outfile.close()
 
 ## Problem 10 - Golf Scores
