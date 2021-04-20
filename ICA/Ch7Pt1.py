b81e572fd83619fbec2e3ac9fe7aa69b36e314cd
John Trujillo <amriikk@gmail.com>
2021-04-19 19:30:19 -0700

    update part 2

--- ICA/Ch7Pt1.py
+++ ICA/Ch7Pt1.py
@@ -9,14 +9,20 @@ def main():
         pickfour[x] = random.randint(0,9)
         x += 1
     # end while loop
-    print('Pickfour elements in order')
+    print('\nPickfour elements in order')
     for x in pickfour:
         print(x, end=' ')
     print()
-    print('Pickfour elements in reverse order')
+    print('\nPickfour elements in reverse order')
     for x in range(-1, -5, -1):
         print(pickfour[x], end=' ')
     print()
 
+    # Part 2
+    OrigSixTeams = ["Canadiens", "Maple Leafs", "Bruins", "Black Hawks", "Red Wings", "Rangers"]
+    OrigSixYears = [1917,1917,1924,1926,1926,1926]
+    NextSixtTeams = ['Kings', 'North Stars', 'Seels', 'Flyers', 'Penguins', 'Blues']
+    print('Original Six NHL teams based in Canada: ', OrigSixTeams[:2])
+    print('Original Six NHL teams based in USA: ', OrigSixTeams[2:])
 
 main()
