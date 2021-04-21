9f57782d6e575743ecd4a5c44ad090fc08b2f971
John Trujillo <amriikk@gmail.com>
2021-04-20 23:45:36 -0700

    Binary burgers

--- Projects/Project2.py
+++ Projects/Project2.py
@@ -4,3 +4,25 @@
 # Assignment: Project 2
 # Due Date: 05.  .21
 # ####################################################
+
+## Binary Burgers ##
+
+# Burger + Sandwich prices:
+hamburger = 2.75        # option [1]
+cheeseburger = 3.25     # option [2]
+sandwich = 2.50         # option [3]
+
+# Side-Order prices:
+fries = 2.25            # option [1]
+rings = 1.75            # option [2]
+salad = 1.50            # option [3]
+
+# Drink prices:
+sm = 1.50               # option [1]
+med = 2.25              # option [2]
+lg = 2.75               # option [3]
+h2o = 0.00              # option [4]
+
+# Total + Tax variables:
+subtotal = 0.00
+TAX = 0.08
