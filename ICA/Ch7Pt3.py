5adac4f3fe3a2d918b9d8972a461acc8602c3ed6
John Trujillo <amriikk@gmail.com>
2021-04-26 18:24:37 -0700

    update Wk 12 ICA 1

---
+++ ICA/Ch7Pt3.py
@@ -0,0 +1,26 @@
+# Week 12, ICA 1
+
+def main():
+    marcel = get_goals(dionne.txt)
+    total = get_total(marcel)
+    print('Marcel Dionne scored', total, 'goals in', len(marcel), 'NHL seasons.')
+
+def get_goals(filename)
+    goals = [] # empty array
+    file = open(filename, 'r')
+    goals = file.readlines() # reads contents of file into list
+    index = 0
+    while index < len(goals):
+        goals[index] = int(goals[index].rstrip('\n'))
+        index += 1
+    return goals
+
+def get_total(a):
+    total = 0
+    for num in a:
+        total += num
+    return total
+
+
+
+main()
