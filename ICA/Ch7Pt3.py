1f178352d50554b169ce509a071eb8865e81cce1
John Trujillo <amriikk@gmail.com>
2021-04-26 18:33:57 -0700

    update store_goals function

--- ICA/Ch7Pt3.py
+++ ICA/Ch7Pt3.py
@@ -4,11 +4,14 @@ def main():
     marcel = get_goals('dionne.txt')
     total = get_total(marcel)
     print('Marcel Dionne scored', total, 'goals in', len(marcel), 'NHL seasons.')
+    bossy1 = [53,69,51,68,64,60,51,58,61,38]
+    store_goals(bossy1, 'bossy1.txt')
 
 def get_goals(filename):
     goals = [] # empty array
     file = open(filename, 'r')
     goals = file.readlines() # reads contents of file into list
+    file.close()
     index = 0
     while index < len(goals):
         goals[index] = int(goals[index].rstrip('\n'))
@@ -20,6 +23,12 @@ def get_total(a):
     for num in a:
         total += num
     return total
+
+def store_goals(g, filename):
+    file = open(filename, 'w')
+    for num in g:
+        file.write(str(num)+'\n')
+    file.close()
 
 
 
