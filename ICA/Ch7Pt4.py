6c5094145a7e62175ba77a0b6933983750500fbc
John Trujillo <amriikk@gmail.com>
2021-04-28 19:54:33 -0700

    update

--- ICA/Ch7Pt4.py
+++ ICA/Ch7Pt4.py
@@ -1,5 +1,5 @@
 # Week 12, ICA 2
-
+import matplotlib.pyplot as plt
 def main():
     premier = ('Liverpool','Chelsea','Arsenal','Southampton')
     mls = ('LAFC','LA Galaxy','DC United','Sporting KC')
@@ -20,5 +20,58 @@ def main():
     print(soccer_teams)
     soccer_teams2 = tuple(soccer_teams)
     print(soccer_teams2)
+
+    # Part 2
+    # Create lists with x, y coordinates
+    plt.figure()
+    x_coords = [0,1,2,3,4]
+    y_coords = [0,3,1,5,2]
+
+    #Build the line graph
+    plt.plot(x_coords, y_coords, marker='o')
+
+    # Add a Title
+    plt.title('Sales by Year')
+
+    # Add labels to the axis
+    plt.xlabel('Year')
+    plt.ylabel('Sales')
+
+    # Customize the tick markers
+    plt.xticks([0,1,2,3,4], ['2016','2017','2018','2019','2020'])
+    plt.yticks([0,1,2,3,4,5], ['$0M','$1M','$2M','$3M','$4M','$5M'])
+
+    # Add a Grid
+    plt.grid(True)
+
+    # Display the line graph
+    # plt.show()
+
+    plt.figure()
+    # Bar Graph - left edges
+    left_edges = [0,10,20,30,40]
+
+    # Bar Height
+    heights = [100,200,300,400,500]
+
+    bar_width = 10
+
+    # Build Bar Chart
+    plt.bar(left_edges, heights, bar_width, color=('r','g','b','m','y'))
+
+    # Add a title 
+    plt.title('Sales by Year')
+
+    # Add labels to axis
+    plt.xlabel('Year')
+    plt.ylabel('Sales')
+
+    # Customize the tick markers
+    plt.xticks([0,15,25,35,45], ['2016','2017','2018','2019','2020'])
+    plt.yticks([0,100,200,300,400,500], ['$0M','$1M','$2M','$3M','$4M','$5M'])
+
+    # Show the bar graph
+    plt.show()
+
 
 main()
