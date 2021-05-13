5814954a25d00ce9c08fe8bf2b187f5f8c4d1cff
John Trujillo <amriikk@gmail.com>
2021-05-12 18:59:16 -0700

    update ica pt 2

---
+++ ICA/Ch9Pt2.py
@@ -0,0 +1,9 @@
+# Week 14, ICA 2
+import pickle
+def main():
+    pacific = set()
+    pacific.add('CA')
+    pacific.add('NV')
+    pacific.add('WA')
+    print('pacific =', pacific)
+    mountain = set(['OR','ID', 'ND', 'SD'])
